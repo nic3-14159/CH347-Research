e2de400905ef571c1954c096b4ff0bc78da537a7
Nicholas Chin <nic.c3.14@gmail.com>
2022-10-20 17:21:17 -0600

    ch347_repl: Add commands for spiCfg and init

--- ch347_repl.c
+++ ch347_repl.c
@@ -2,9 +2,38 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "CH347DLL_EN.H"
 
 #define LINE_LENGTH 1024
+
+static void set_spiCfg(mSpiCfgS *cfg, char *field, unsigned long value) {
+	if (strcmp(field, "iMode") == 0) {
+		cfg->iMode = (unsigned char) value;
+	} else if (strcmp(field, "iClock") == 0) {
+		cfg->iClock = (unsigned char) value;
+	} else if (strcmp(field, "iByteOrder") == 0) {
+		cfg->iByteOrder = (unsigned char) value;
+	} else if (strcmp(field, "iSpiWriteReadInterval") == 0) {
+		cfg->iSpiWriteReadInterval = (unsigned short) value;
+	} else if (strcmp(field, "iSpiOutDefaultData") == 0) {
+		cfg->iSpiOutDefaultData = (unsigned char) value;
+	} else if (strcmp(field, "iChipSelect") == 0) {
+		cfg->iChipSelect = (unsigned long) value;
+	} else if (strcmp(field, "CS1Polarity") == 0) {
+		cfg->CS1Polarity = (unsigned char) value;
+	} else if (strcmp(field, "CS2Polarity") == 0) {
+		cfg->CS2Polarity = (unsigned char) value;
+	} else if (strcmp(field, "iIsAutoDeativeCS") == 0) {
+		cfg->iIsAutoDeativeCS = (unsigned short) value;
+	} else if (strcmp(field, "iActiveDelay") == 0) {
+		cfg->iActiveDelay = (unsigned short) value;
+	} else if (strcmp(field, "iDelayDeactive") == 0) {
+		cfg->iDelayDeactive = (unsigned long) value;
+	} else {
+		printf("Bad value\n");
+	}
+}
 int main(int argc, char** argv) {
 	ULONG i = 0;
 	BOOL ret = FALSE;
@@ -21,6 +50,9 @@ int main(int argc, char** argv) {
 
 	char input_buffer[LINE_LENGTH];
 	char *args[32] = {NULL};
+	char *data_buf = NULL;
+	unsigned long cs = 0;
+	unsigned long o;
 	while (1) {
 		printf("CH347: ");
 		fflush(stdout);
@@ -57,13 +89,18 @@ int main(int argc, char** argv) {
 			CH347SPI_SetChipSelect(0, mask, cs, 0, 0, 0);
 			
 		} else if (strcmp(input_buffer, "r") == 0) {
-			continue;	
+			continue;
 		} else if (strcmp(input_buffer, "w") == 0) {
-			continue;	
+			continue;
 		} else if (strcmp(input_buffer, "wr") == 0) {
-			continue;	
-		} else if (strcmp(input_buffer, "set") == 0) {
-			continue;	
+			continue;
+		} else if (strcmp(input_buffer, "spicfg") == 0) {
+			unsigned long val = strtol(args[2], NULL, 16);
+			set_spiCfg(&spiCfg, args[1], val);
+			continue;
+		} else if (strcmp(input_buffer, "init") == 0) {
+			ret = CH347SPI_Init(0, &spiCfg);
+			printf(!ret ? " Error! CH347SPI_Init\n" : "");
 		} else {
 			printf("Unknown command: %s\n", input_buffer);
 		}
